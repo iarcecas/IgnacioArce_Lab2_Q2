eb68228ec6d4ae7b7e275ba6ae0621d3c0a4752b
Ignacio Arce <iarcecas@my.centennialcollege.ca>
2025-02-16 16:00:08 -0500

    Welcome to Good Morning/Afternoon/Evening

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,18 @@
 package org.example;
 
+import java.time.LocalTime;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to COMP367!");
+        LocalTime currentTime = LocalTime.now();
+        if (currentTime.isAfter(LocalTime.of(5, 0)) && currentTime.isBefore(LocalTime.of(12, 0))) {
+            System.out.println("Good morning Ignacio! Welcome to COMP367");
+        } else if (currentTime.isAfter(LocalTime.of(12, 0)) && currentTime.isBefore(LocalTime.of(17, 0))) {
+            System.out.println("Good afternoon Ignacio! Welcome to COMP367");
+        } else {
+            System.out.println("Good evening Ignacio! Welcome to COMP367");
+        }
     }
 }
